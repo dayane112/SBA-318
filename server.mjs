1e8764f3d8db244339d01d72d97de2ecff8c45ef
dayane112 <71454308+dayane112@users.noreply.github.com>
2024-10-11 12:06:01 -0400

    Initial commit

---
+++ server.mjs
@@ -0,0 +1,20 @@
+// Imports
+import express from "express";
+
+// nitialize express and PORT number 
+const app = express();
+let PORT = 3000;
+
+
+// middleware
+
+
+
+// Routes
+
+
+
+// listen
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`)
+})
