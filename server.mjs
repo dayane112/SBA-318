9a5b09030f40dc1c331921f6c828edda4a88d125
dayane112 <71454308+dayane112@users.noreply.github.com>
2024-10-14 22:11:13 -0400

    Adding pug files

--- server.mjs
+++ server.mjs
@@ -3,15 +3,35 @@ import express from "express";
 import patientsRoutes from "./routes/patientsRoutes.mjs";
 import doctorsRoutes from "./routes/doctorsRoutes.mjs";
 import patient1Routes from "./routes/patient1Routes.mjs";
+import { patients } from './data/patients.mjs';
+import { doctors } from './data/doctors.mjs';
+
+import pug from "pug";
 
 // Initialize express and PORT number 
 const app = express();
 let PORT = 3000;
 
+// Servin static files
+app.use(express.static('./styles'));
+
 
 // middleware
 app.use(express.json());
 
+
+app.set("view engine", "pug");
+app.get("/", (req, res) => {
+  res.render("index"); //filename home.pug
+});
+
+app.get("/patients", (req, res) => {
+    res.render("patientList", { patients: patients }); //filename home.pug
+});
+
+app.get("/doctors", (req, res) => {
+    res.render("doctorList", { doctors: doctors }); //filename home.pug
+});
 
 
 
